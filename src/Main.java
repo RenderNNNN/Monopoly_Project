189412c4471a02da11c236ece808140e7656fc57
skt29 <sarahkt29@gmail.com>
2022-10-19 12:16:19 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         //Commit worked if you're reading this
-
+        System.out.println("Welcome!");
 
     }
 }

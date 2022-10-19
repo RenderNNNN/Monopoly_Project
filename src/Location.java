e54c9fb0cb1ac70d1d5019fddb976fa52ba5e524
nathanvits <nfvits@icloud.com>
2022-10-19 12:07:52 -0500

    Committing all the classes

---
+++ src/Location.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+class Location<T>{
+    public T data;
+
+    public Location<?> previous;
+    public Location<?> next;
+
+    //public ArrayList<Player> player = new ArrayList<>();
+
+    public boolean tagged = false;
+
+    public Location(T data){
+        this.data = data;
+        previous = null;
+        next = null;
+    }
+
+    public void receive(Player player){
+        if(data instanceof Property x){
+            //Property property = (Property) data;
+            if(!x.owner.equals(player)){
+                x.collectRentFrom();
+            }
+        }
+        else if(data instanceof Railroad){
+
+        }
+        else if()
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+public class Player{
+    /*
+    0) Purple
+    1) Light Blue
+    2) Pink
+    3) Orange
+    4) Red
+    5) Yellow
+    6) Green
+    7) Dark Blue
+     */
+    public String name;
+    public int balance;
+    public Location<?> location;
+
+    public Player(String name, int balance){
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void advance(){
+        //Rolls die, updates location,
+    }
+
+}

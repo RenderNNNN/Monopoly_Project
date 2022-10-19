886ac1bedb4020b4b66f38628a2081cb4a8d5991
skt29 <sarahkt29@gmail.com>
2022-10-19 12:39:28 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main{
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome! Please enter the total number of players");
         int totalPlayerCount = input.nextInt();
-        for (int i = 0; i < totalPlayerCount; i++){
-
+        for (int i = 1; i <= totalPlayerCount; i++){
+            System.out.println("Player");
         }
     }
 }

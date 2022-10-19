c0994bd106afc5a4eb8b988a470ca1d24b981cca
skt29 <sarahkt29@gmail.com>
2022-10-19 12:26:28 -0500

    test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        //Commit worked if you're reading this
-        //Test
-        //test
-        //
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome! Please enter the total number of players");
+        int totalPlayerCount = input.nextInt();
+        for (int i = 0; i < totalPlayerCount; i++){
+
+        }
     }
 }
